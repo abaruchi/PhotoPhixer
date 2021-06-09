12fc54740cd0382df89de7f43c9577d92bbced7e
Artur Baruchi <artur.baruchi@gmail.com>
2021-06-09 15:23:34 -0300

    Adds SysConfig() unit test

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -7,29 +7,15 @@ from pathlib import Path
 class TestConfig(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.configuration = config.SysConfig()
-        self.custom_db_file = Path(getcwd()) / 'test/db_dir/test_db_file.db'
-        self.custom_log_file = Path(getcwd()) / 'test/log_dir/'
+        test_file = Path(getcwd()) / 'test_config.ini'
+        self.configuration = config.SysConfig(test_file)
 
-    def test_default_configuration(self):
-        self.assertTrue(self.configuration.db_file.parent.exists())
-        self.assertTrue(self.configuration.log_dir.exists())
-
-    def test_custom_configuration(self):
-        # Stores old config files to cleanup
-        self.old_log_dir = self.configuration.log_dir
-        self.old_db_file = self.configuration.db_file
-
-        # Set custom configurations
-        self.configuration.log_dir = str(self.custom_log_file)
-        self.configuration.custom_db_file = str(self.custom_db_file)
-
-        self.assertTrue(self.configuration.log_dir.exists())
-        self.assertTrue(self.configuration.db_file.parent.exists())
-
-    def tearDown(self) -> None:
-        self.old_log_dir.rmdir()
-        self.old_db_file.parent.rmdir()
-
-        self.configuration.log_dir.rmdir()
-        self.configuration.db_file.parent.rmdir()
+    def test_config_dict(self):
+        config_dict = {'DEFAULT': {},
+                       'GLOBAL':
+                           {'debug': 'False',
+                            'dropbox_key': "'some_randon_key_here'",
+                            'log_file': '/var/log/',
+                            'sqlite_path': 'data/SQLite/photophixer.db'}
+                       }
+        self.assertDictEqual(self.configuration.list_config(), config_dict)
