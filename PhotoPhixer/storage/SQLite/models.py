a967835d9e428f056adbe58a4468a1020992b99e
Artur Baruchi <artur.baruchi@gmail.com>
2021-06-21 09:11:57 -0300

    Refactors ORM models to be able to inject configuration class

--- PhotoPhixer/storage/SQLite/models.py
+++ PhotoPhixer/storage/SQLite/models.py
@@ -1,43 +1,72 @@
 from datetime import datetime
+from os import getcwd
+from pathlib import Path
 
-from PhotoPhixer.common.config import main_config, SysConfig
-from pony.orm import (Database, Optional, PrimaryKey, Required, Set)
+from pony.orm import Database, Optional, PrimaryKey, Required, Set
+
+from PhotoPhixer.common.config import SysConfig
 
 
-def create_db_connection(config: SysConfig) -> Database:
+def db_connection(config: SysConfig) -> Database:
     """
+    This routine must be used to create and manage data in database.
 
-    :param config:
-    :return:
+    :param config: A config instance where important DB properties must be set
+    :return: A database connection to handle data
     """
     config_dict = config.list_config()
-    return Database(
+    sqlite_path = Path(getcwd()) / config_dict['GLOBAL']['sqlite_path']
+
+    db = Database(
         config_dict['GLOBAL']['db_engine'],
-        config_dict['GLOBAL']['sqlite_path'],
+        str(sqlite_path),
         create_db=True)
 
+    class File(db.Entity):
+        id = PrimaryKey(str)
+        name = Optional(str)
+        file_type = Optional(str)
+        device = Optional(str)
+        has_metadata = Optional(bool)
+        date_processing = Optional(datetime)
+        date_file_creation = Optional(datetime)
+        date_last_change = Optional(datetime)
+        dropbox_hash = Optional(str)
+        directory = Optional('Directory')
+
+    class Directory(db.Entity):
+        id = PrimaryKey(str)
+        path = Required(str)
+        date_creation = Required(datetime)
+        date_last_update = Optional(datetime)
+        files = Set(File)
+
+    db.generate_mapping(create_tables=True)
+    add_null_objects(db)
+
+    return db
 
 
+def add_null_objects(db: Database) -> None:
+    """
+    This routine creates null objects into Database so we can return these
+    objects instead of raising an error or different objects types.
+    :param db: Database connection
+    :return: None
+    """
 
-class File(create_db_connection.Entity):
-    id = PrimaryKey(str)
-    name = Optional(str)
-    file_type = Optional(str)
-    device = Optional(str)
-    has_metadata = Optional(bool)
-    date_processing = Optional(datetime)
-    date_file_creation = Optional(datetime)
-    date_last_change = Optional(datetime)
-    dropbox_hash = Optional(str)
-    directory = Optional('Directory')
+    if not db.File.exists(id='None'):
+        null_file = db.File(
+            id='None',
+            name='None',
+            file_type='None'
+        )
 
-
-class Directory(db.Entity):
-    id = PrimaryKey(str)
-    path = Required(str)
-    date_creation = Required(datetime)
-    last_update = Optional(datetime)
-    files = Set(File)
-
-
-db.generate_mapping(create_tables=True)
+    if not db.Directory.exists(id='None'):
+        db.Directory(
+            id='None',
+            path='None',
+            date_creation=datetime.now(),
+            last_update=datetime.now(),
+            files=null_file
+        )
